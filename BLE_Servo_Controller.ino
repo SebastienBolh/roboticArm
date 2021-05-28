7d9da1ada5cd0bb20e25bd4be58a77da493ab25b
Sebastien Bolh <76074256+SebastienBolh@users.noreply.github.com>
2021-05-28 11:33:38 -0400

    V3 Four Servo Control
    Four servos in control, breadboard used to power servos as Arduino has run out of space.

--- BLE_Servo_Controller.ino
+++ BLE_Servo_Controller.ino
@@ -5,9 +5,10 @@
 #include <Servo.h>
 #include <Dabble.h>
 
-Servo s0, s1;
+//servos, s2/s3 are high-torque
+Servo s0, s1, s2, s3;
 int LED = 13;
-int s0A = 0, s1A = 0;
+int s0A = 0, s1A = 0, s2A = 0, s3A = 0;
 bool bytesWereRead = false;
 char c = ' ';
 boolean NL = true;
@@ -22,14 +23,16 @@ void setup() {
   //visual LED to indicate function
   pinMode(LED, OUTPUT);
   digitalWrite(LED, HIGH);
-  pinMode(9, OUTPUT);
 
   //setup servos
   s0.attach(9);
   s0.write(0);
   s1.attach(10);
   s1.write(0);
-
+  s2.attach(11);
+  s2.write(0);
+  s3.attach(12);
+  s3.write(0);
 }
 
 void loop() {
@@ -47,8 +50,33 @@ void loop() {
     delay(200);
     sFactor+=1;
   }
-  if(GamePad.isCirclePressed()){
-    Serial.println("Circle");  
+  //increase s2 angle
+  if(GamePad.isSquarePressed() && s2A<180){
+    Serial.println("Square");
+    s2A+=(sFactor);
+    s2.write(s2A);
+    delay(20);
+  }
+  //decrease s2 angle
+  if(GamePad.isCirclePressed() && s2A>0){
+    Serial.println("Circle");
+    s2A-=(sFactor);
+    s2.write(s2A);
+    delay(20);
+  }
+  //increase s3 angle
+  if(GamePad.isTrianglePressed() && s3A<180){
+    Serial.println("Triangle");
+    s3A+=(sFactor);
+    s3.write(s3A);
+    delay(20);
+  }
+  //decrease s3 angle
+  if(GamePad.isCrossPressed() && s3A>0){
+    Serial.println("Cross");
+    s3A-=(sFactor);
+    s3.write(s3A);
+    delay(20);
   }
   //increase s0 angle
   if(GamePad.isUpPressed() && s0A<180){
